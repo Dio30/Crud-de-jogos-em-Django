8b28a0ba86f686adbafc2ca6560374dff6d036fb
Dione <padilhadione1@gmail.com>
2022-06-03 12:32:31 -0300

    fix models

--- jogos/models.py
+++ jogos/models.py
@@ -1,11 +1,12 @@
+from turtle import width
 from django.db import models
 from django.contrib.auth.models import User
 
 class Jogos(models.Model):
-    nome_do_jogo = models.CharField(max_length=30, verbose_name='nome_do_jogo')
+    nome_do_jogo = models.CharField(max_length=30)
     tipo_de_jogo = models.CharField(max_length=30)
-    valor_do_jogo = models.DecimalField(max_digits=7, decimal_places=2, null=True)
-    descrição_do_jogo = models.TextField(null=True, blank=True)
+    valor_do_jogo = models.DecimalField(max_digits=7, decimal_places=2)
+    descrição_do_jogo = models.CharField(max_length=100, null=True, blank=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     
     class Meta:
