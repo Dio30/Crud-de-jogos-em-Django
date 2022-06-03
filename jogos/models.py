266bb1657ec71d2b35214b52fab3321e81d4be9d
Dione <padilhadione1@gmail.com>
2022-06-03 13:10:41 -0300

    fix all

--- jogos/models.py
+++ jogos/models.py
@@ -5,7 +5,7 @@ class Jogos(models.Model):
     nome_do_jogo = models.CharField(max_length=30)
     tipo_de_jogo = models.CharField(max_length=30)
     valor_do_jogo = models.DecimalField(max_digits=7, decimal_places=2)
-    descrição_do_jogo = models.CharField(max_length=100, null=True, blank=True)
+    descrição_do_jogo = models.CharField(max_length=100, null=True, blank=False)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     
     class Meta:
